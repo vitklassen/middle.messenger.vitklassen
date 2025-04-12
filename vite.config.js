77679199b0ffab6b37fb7c55e062a6fec28b1f77
vitklassen <vit.klassen@yandex.ru>
2025-04-12 15:41:06 +0500

    fix errors v.1

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,14 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: resolve(__dirname, 'index.html'),
-                page_not_found_error: resolve(__dirname, 'page-not-found-error.html'),
-                page_server_error: resolve(__dirname, 'page-server-error.html'),
-                page_signin: resolve(__dirname, 'page-signin.html'),
-                page_sigup: resolve(__dirname, 'page-signup.html'),
-                page_profile: resolve(__dirname, 'page-profile.html'),
-                page_profile_change_data: resolve(__dirname, 'page-profile-change-data.html'),
-                page_profile_change_password: resolve(__dirname, 'page-profile-change-password.html'),
-                page_chats: resolve(__dirname, 'page-chats.html'),
+                page_not_found_error: resolve(__dirname, 'src/pages/page-not-found-error.html'),
+                page_server_error: resolve(__dirname, 'src/pages/page-server-error.html'),
+                page_signin: resolve(__dirname, 'src/pages/page-signin.html'),
+                page_sigup: resolve(__dirname, 'src/pages/page-signup.html'),
+                page_profile: resolve(__dirname, 'src/pages/page-profile.html'),
+                page_profile_change_data: resolve(__dirname, 'src/pages/page-profile-change-data.html'),
+                page_profile_change_password: resolve(__dirname, 'src/pages/page-profile-change-password.html'),
+                page_chats: resolve(__dirname, 'src/pages/page-chats.html'),
             }
         },
         outDir: resolve(__dirname, 'dist'),
