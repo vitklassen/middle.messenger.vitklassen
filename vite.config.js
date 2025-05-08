c7b7387e403755eb3dbc878d59799cdc1e7eedac
Viktor Klassen <vit.klassen@yandex.ru>
2025-05-08 23:58:54 +0500

    fix mistake v.4

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                index: resolve(__dirname, 'index.html'),
-            }
-        },
-        outDir: resolve(__dirname, 'dist'),
-        emptyOutDir: true,
-    },
-    server: {
-        port: 3000
-    },
+  build: {
+      rollupOptions: {
+          input: {
+              index: resolve(__dirname, 'index.html'),
+          },
+      },
+      outDir: resolve(__dirname, 'dist'),
+      emptyOutDir: true,
+  },
+  server: {
+      port: 3000,
+  },
 });
