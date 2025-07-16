cb014a50fd651baca830eb5cd1d8c536e1401e47
Viktor Klassen <vit.klassen@yandex.ru>
2025-07-16 20:44:28 +0500

    add base tests for Component

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,8 +1,10 @@
-const { JSDOM } = require('jsdom');
+import {JSDOM} from 'jsdom';
 
 const jsdom = new JSDOM('<body></body>', {
     url: 'https://example.org/',
 });
 
 global.window = jsdom.window;
-global.document = jsdom.document;
+global.document = jsdom.window.document;
+global.HTMLElement = jsdom.window.HTMLElement;
+global.MouseEvent = jsdom.window.MouseEvent;
